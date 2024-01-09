e47dd669ef8444e7f96c863cd2c899897b14cff3
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-08 18:35:08 -0800

    add functionality

--- verify.js
+++ verify.js
@@ -4,10 +4,8 @@ codes[0].focus()
 
 codes.forEach((code, index) => {
   code.addEventListener('keydown', (e) => {
-    if(e.key >= 0 && e.key <=9) {
-
+    if (e.key >= 0 && e.key <= 9) {
       codes[index].value = ''
-
       setTimeout(() => codes[index + 1].focus(), 10)
     } else if (e.key === 'Backspace') {
       setTimeout(() => codes[index - 1].focus(), 10)
